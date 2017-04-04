1c8cf69a7d5d40666029f644511806f7fb736495
Chaoran Chen <chaoranchen@Chaorans-MacBook-Pro.local>
2017-04-03 20:04:29 -0500

    Week 9

--- PriorityQueue/CarSim.java
+++ PriorityQueue/CarSim.java
@@ -7,33 +7,35 @@
 
 public class CarSim {
 
-    // public variables
+	// public variables
 
-    static PQ agenda = new PQ();
-    static Washer washer;  // the car washer
+	static PQ agenda = new PQ();
+	//static Washer washer;  // the car washer
 
-    // methods
+	// methods
 
-    public static void main(String args[]) {
+	public static void main(String args[]) {
 
-        int distArray[] = {20, 40};
-        
+		int distArray[] = {20, 40};
+		
 //      first parameter to CarMaker in line below is arrival rate 
 //        adjust this parameter to change load on the system
 //        and determine equilibrium  
 
-        agenda.add(new CarMaker(38, distArray), 10);
+		agenda.add(new CarMaker(38, distArray), 10);
 
-//      washer = new Washer();
-        
+		
+
+//      washer = new Washer();agenda.add(washer, 5);
+		
 //      adjust simulation time to determine is system is at equilibrium
 
-        while (agenda.getCurrentTime() <= 15000)
-            agenda.remove().run();
+		while (agenda.getCurrentTime() <= 15000)
+			agenda.remove().run();
 
-        Stat.displayStats();
+		Stat.displayStats();
 
-    }  // main method
-    
+	}  // main method
+	
 
 }  // CarSim class
